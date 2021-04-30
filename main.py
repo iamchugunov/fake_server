927637eeacaea4b1eccb65f8d31993d85fc1813c
Natasha Masalkova <64376822+nawas5@users.noreply.github.com>
2021-04-30 17:17:31 +0300

    Тутачки
    ХАХА

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import socket
-import data
 import json
+import time
+
+
 
 HEADER = 64
 PORT = 5050
-SERVER = '192.168.43.54'
+SERVER = '192.168.43.43'
 ADDR = (SERVER, PORT)
 
 
@@ -20,13 +22,14 @@ mes["az"] = 0.
 mes["hei"] = 0.
 mes["wind_module"] = 0.
 mes["wind_direction"] = 0.
-mes["bullet_type"] = 3
+mes["bullet_type"] = 2
 mes["temperature"] = 20
-mes["pressure"] = 798
+mes["atm_pressure"] = 798
 
 meas = []
-f = open('82_17-02.txt', 'r')
+f = open('762_16-15.txt', 'r')
 for line in f:
+
     a = line.split()
     poit = {}
     poit["execTime_sec"] = float(a[1])
@@ -40,6 +43,7 @@ for line in f:
     poit["sVr"] = 0.
     poit["Amp"] = float(a[2])
     meas.append(poit)
+
 f.close()
 
 meas_dict = {}
@@ -69,6 +73,7 @@ while True:
         conn.sendall((0x150001).to_bytes(4, "little"))
         conn.sendall(data2send)
     elif data1 == "2":
+        t0 = time.time()
         data2send = json.dumps(meas_dict).encode()
         print(len(data2send))
         data = json.loads(data2send.decode())
@@ -76,4 +81,27 @@ while True:
         conn.sendall(len(data2send).to_bytes(4, "little"))
         conn.sendall((0x150002).to_bytes(4, "little"))
         conn.sendall(data2send)
+
+        while True:
+
+            rcv_size = int.from_bytes(conn.recv(4), "little")
+            rcv_type = int.from_bytes(conn.recv(4), "little")
+
+            print("Size {}".format(rcv_size))
+            print("Type {:0x}".format(rcv_type))
+
+            data = conn.recv(rcv_size)
+            last_bytes = rcv_size - len(data)
+
+            while last_bytes > 0:
+                data = data + conn.recv(last_bytes)
+                last_bytes = rcv_size - len(data)
+
+            if rcv_type == 0x150003:
+                data = json.loads((data.decode()))
+                print(data, "track config")
+                print(time.time() - t0, "время обработки в секундах")
+
+
+
 
