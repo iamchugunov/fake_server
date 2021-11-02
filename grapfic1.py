7a9e1dfd318b154faa18e2df56a4fd70651af189
Natalya <natasha.masalckova@yandex.ru>
2021-11-02 15:36:35 +0300

    новые данные

--- grapfic1.py
+++ grapfic1.py
@@ -1,6 +1,6 @@
 import plotly.graph_objs as go
 from plotly.subplots import make_subplots
-
+import json
 import matplotlib.pyplot as plt
 #pio.renderers.default = 'svg'
 
@@ -9,29 +9,13 @@ import os
 
 directory_folder = os.getcwd()
 
-file_name = '3poi-rszo'
-f = open('logs/' + file_name + '.txt', 'r')
+file_name = 'trackdata_bullet'
+
+with open(file_name + '.json', 'r') as file:
+    data = json.load(file)
 
 # передавать количетво точек? где разрывы и где нет
-points = []
-
-for line in f:
-
-    a = line.split()
-    poit = {}
-    poit["execTime"] = float(a[1])
-    poit["Beta"] = 0.
-    poit["sBeta"] = 0.
-    poit["Epsilon"] = float(a[6]) #6 - rszo
-    poit["sEpsilon"] = 0.
-    poit["R"] = float(a[3])
-    poit["sR"] = 0.
-    poit["Vr"] = abs(float(a[4]))
-    poit["sVr"] = 0.
-    poit["Amp"] = float(a[2])
-    points.append(poit)
-
-f.close()
+points = data['points']
 
 meas_dict = {}
 meas_dict["points"] = points
@@ -48,22 +32,7 @@ for i in range(len(meas_dict["points"])):
     Vr.append(meas_dict["points"][i]["Vr"])
     E.append(meas_dict["points"][i]["Epsilon"])
 
-A = []
-for i in range(len(Vr) - 1):
-    A.append((Vr[i+1] - Vr[i])/(t[i+1] - t[i]))
 
-time_in = 0
-
-for i in range(len(A)):
-    if A[i] < 100:
-        time_in = i
-        break
-
-print(time_in)
-print(A)
-
-
-plt.plot(t, R)
 trace_R = go.Scatter(x=t, y=R, name='расстояние', mode='markers')
 trace_Vr = go.Scatter(x=t, y=Vr, name='радиальная скорость относительно локатора', mode='markers')
 trace_E = go.Scatter(x=t, y=E, name='угол', mode='markers')
@@ -74,11 +43,10 @@ fig.add_trace(trace_R, row=1, col=1)
 fig.add_trace(trace_Vr, row=2, col=1)
 fig.add_trace(trace_E, row=3, col=1)
 
-fig.update_layout(title_text="Графики файла " + file_name)
 
 fig.update_traces(hoverinfo="all", hovertemplate="x: %{x}<br>y: %{y}")
 
-fig.write_html('result/12' + file_name + '.html')
+fig.write_html('result/12.html')
 # добавлять в название
 
 # применение фильтра для угла
